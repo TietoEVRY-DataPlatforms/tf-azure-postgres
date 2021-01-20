b91e571fd21f781ebd0275b592245f0d376a20b4
Endre Karlson <endre.karlson@gmail.com>
2021-01-20 22:55:36 +0100

    Don't bother to enable sec if Basic SKU

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_postgresql_server" "server" {
   storage_mb                   = var.storage_mb
   backup_retention_days        = var.backup_retention_days
   geo_redundant_backup_enabled = var.geo_redundant_backup
-  auto_grow_enabled = var.auto_grow_enabled
+  auto_grow_enabled            = var.auto_grow_enabled
 
   administrator_login              = var.administrator_login
   administrator_login_password     = var.administrator_password
@@ -22,14 +22,14 @@ resource "azurerm_postgresql_server" "server" {
   ssl_minimal_tls_version_enforced = var.ssl_minimal_tls_version_enforced
   tags                             = var.tags
 
-  threat_detection_policy  {
-   enabled = (var.sku_name != "B*" ? true : false)
-   email_account_admins = true
-   email_addresses = ["dp-platform-team@tietoevry.com"]
-   #disabled_alerts = ["",]
-   #retention_days = 365
-   #storage_account_key =
-   #storage_endpoint = 
+  threat_detection_policy {
+    enabled              = length(regexall("^B.*", var.sku_name)) == 0 ? true : false
+    email_account_admins = true
+    email_addresses      = ["dp-platform-team@tietoevry.com"]
+    #disabled_alerts = ["",]
+    #retention_days = 365
+    #storage_account_key =
+    #storage_endpoint =
   }
 }
 
